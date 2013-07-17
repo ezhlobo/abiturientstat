a1fe515bbdac053a0ad9947b5bec23ecace3f662
Evgeny Zhlobo <evgen.zhlobo@gmail.com>
2013-07-18 02:32:42 +0300

    Get table with scores

--- backend/application.rb
+++ backend/application.rb
@@ -2,13 +2,24 @@ require 'sinatra/base'
 require 'sinatra/json'
 require 'sinatra/namespace'
 
+require 'open-uri'
+require 'iconv'
+
 class AbiturientStat < Sinatra::Base
   register Sinatra::Namespace
   helpers Sinatra::JSON
 
   namespace '/backend' do
-    get '/' do
-      json hello: 'world'
+    get '/table' do
+      url = "http://abitur.bsuir.by/stat/budjet.jsp"
+      page = open(url)
+      source = page.read.scan(/<table cellspacing="0" align="center">.*<\/table>/m)[0]
+      parameters = page.content_type_parse
+
+      encoding = parameters.assoc('charset').last
+
+      converter = Iconv.new('UTF-8//IGNORE', encoding)
+      converter.iconv(source)
     end
   end
 
